b6d015074c5145c5aff8e0427e53e00946796e7b
Christina Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2025-08-27 19:56:07 +0100

    Implement a function called validateEmail($email) that uses a regular expression to check if an email address is valid.

---
+++ email_validator.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Project: Email Validator
+ * 
+ * Description: An email validator tool that checks mulitple email addresses entered by users are valid or not.
+ */
+
+/**
+ * Validates an email address using a regular expression.
+ * @param string $email The email address to validate.
+ * @return bool True if the email address is valid, false otherwise.
+ */
+function validateEmail($email) {
+  // Regular expression for baisc email validation
+  $regex = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/i";
+  
+  if (preg_match($regex, $email)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+?>
